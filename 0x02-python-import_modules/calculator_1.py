c467820fa360917686579ff9176b7c6212b975c8
tarkavs <tarkavsunday@outlook.com.com>
2022-12-01 20:03:40 -0800

    my commit

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -1,30 +1,38 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 
-def add(a,b):
-    """My add function
-    Args:
-    a: first integer
-    b: second integer
-    Returns:
-    The return value. a + b
-    """
-    return (a + b)
+def add(a, b):
+    """My addition function
 
-def sub(a, b):
-    """My subtraction function
     Args:
         a: first integer
         b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
     Returns:
         The return value. a - b
     """
     return (a - b)
 
+
 def mul(a, b):
     """My multiplication function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a * b
     """
@@ -33,9 +41,11 @@ def mul(a, b):
 
 def div(a, b):
     """My division function
+
     Args:
         a: first integer
         b: second integer
+
     Returns:
         The return value. a / b
     """
